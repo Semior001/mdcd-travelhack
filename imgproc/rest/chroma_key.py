3c1dc7a6f1a726c4f2a738e9a460053f42feca81
Abuzyar Tazetdinov <tazetdinov9925@gmail.com>
2020-02-08 22:53:10 +0300

    Filtering and Replacing almost done!

--- imgproc/rest/chroma_key.py
+++ imgproc/rest/chroma_key.py
@@ -3,6 +3,9 @@ import base64
 from flask import Blueprint, request
 from PIL import Image
 import io
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class ChromaKeyingService:
@@ -14,6 +17,15 @@ class ChromaKeyingService:
         :param bg_image_str: string which represents a background image
         :return: string which represents a processed image
         """
+        img = cv2.imread(src_image_str)
+        RED, GREEN, BLUE = (2, 1, 0)
+        reds = img[:, :, RED]
+        greens = img[:, :, GREEN]
+        blues = img[:, :, BLUE]
+        mask = (greens < 70) | (reds > greens) | (blues > greens)
+        result = np.where(mask, 255, 0)
+        # TODO background replacing
+        # cv2.imwrite("./test.jpg", result)
         raise NotImplementedError()
 
 
--- imgproc/rest/filtering.py
+++ imgproc/rest/filtering.py
@@ -2,7 +2,8 @@ import base64
 
 from flask import Blueprint, request
 from PIL import Image
-import io
+import pilgram
+import pilgram.css
 
 
 class FilteringService:
@@ -14,6 +15,43 @@ class FilteringService:
         :param filter_name: string which represents a filter
         :return: string which represents a processed image
         """
+        getFilterFn = {
+            "_1977": pilgram._1977,
+            "aden": pilgram.aden,
+            "brannan": pilgram.brannan,
+            "brooklyn": pilgram.brooklyn,
+            "clarendon": pilgram.clarendon,
+            "earlybird": pilgram.earlybird,
+            "gingham": pilgram.gingham,
+            "hudson": pilgram.hudson,
+            "inkwell": pilgram.inkwell,
+            "kelvin": pilgram.kelvin,
+            "lark": pilgram.lark,
+            "lofi": pilgram.lofi,
+            "maven": pilgram.maven,
+            "mayfair": pilgram.mayfair,
+            "moon": pilgram.moon,
+            "nashville": pilgram.nashville,
+            "perpetua": pilgram.perpetua,
+            "reyes": pilgram.reyes,
+            "rise": pilgram.rise,
+            "slumber": pilgram.slumber,
+            "stinson": pilgram.stinson,
+            "toaster": pilgram.toaster,
+            "valencia": pilgram.valencia,
+            "walden": pilgram.walden,
+            "willow": pilgram.willow,
+            "xpro2": pilgram.xpro2,
+            "css-contrast": pilgram.css.contrast,
+            "css-grayscale": pilgram.css.grayscale,
+            "css-hue_rotate": pilgram.css.hue_rotate,
+            "css-saturate": pilgram.css.saturate,
+            "css-sepia": pilgram.css.sepia,
+        }
+        if filter_name not in getFilterFn.keys():
+            raise ValueError  # TODO
+        im = Image.open(src_image_str)
+        getFilterFn[filter_name](im).save()
         raise NotImplementedError()
 
 
